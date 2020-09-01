73d98a4f0894259d1bc96dfe446755011ddb6b02
Jesus Delmoral <jdelmoral@students.mchenry.edu>
2020-08-31 19:43:09 -0500

    Initial commit

---
+++ src/op.kt
@@ -0,0 +1,37 @@
+fun main(){
+    // Arithmetic operators(+,-, *, /, %)
+    var a = 11
+    var b = 5
+
+    var ansl = 0
+    ansl = a % b
+    print(ansl)
+
+    // Comparison operator (==, !=, <, >, <=, >=)
+    var isCompare = a != b
+    print(isCompare)
+
+    // Assignment operators(+=, -=, *=, /=, %=)
+    var ourAnswer = 0
+    ourAnswer = ourAnswer + 1
+    ourAnswer += 5
+    println(ourAnswer)
+    // Increment & Decrement operators (++, --)
+    ourAnswer--
+    println(ourAnswer)
+
+    //homework
+    var art = 0
+    art = 96 - 17 + 38
+    print(art)
+
+    var compare_thsi_rn = a <= b
+    print(compare_thsi_rn)
+
+    var aAnswer = 15
+    aAnswer -= 5
+    println(aAnswer)
+
+    aAnswer++
+    println(aAnswer)
+}
